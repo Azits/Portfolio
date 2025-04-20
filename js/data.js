5472e9b9e00f762137c655e7f03158c8c1b17613
Abdoul-Anziz <mmkanziz@gmail.com>
2025-04-20 16:41:55 +0200

    modification (ajout Python et SQL

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ const data = [
       nom: "Portfolio",
       categorie: "Développement Web",
       Image: "./img/Portfolio.png",
-      description: "Un portfolio interactif et moderne conçu pour présenter mes compétences en développement web.",
+      description: "Un portfolio conçu pour présenter mes compétences en développement web.",
       github: "https://github.com/Azits/portfolio"
     },
     {
@@ -14,19 +14,31 @@ const data = [
       github: "https://github.com/Azits/Pirate-D-fense"
     },
     {
+      nom: "Jeu Echecs et Dames",
+      categorie: "Développement java",
+      Image: "./img/jeuEchecs.png",
+      description: "Jeu d’échecs et dames en Java avec le modèle MVC & Design Patterns Décorateur",
+      github: "https://github.com/Azits/jeux-echecs"
+    },
+    {
       nom: "Simulation Virus",
       categorie: "Développement C++",
       Image: "./img/IMG.png",
       description: "Simulation graphique de propagation d’un virus en C++ avec SFML.",
       github: "https://github.com/Azits/simulation-virus"
     }
+
   ];
   const technologie = [
-    { nom: "HTML", Image: "./img/html5.svg" },
-    { nom: "CSS", Image: "./img/css.svg" },
-    { nom: "JavaScript", Image: "./img/javascript.svg" },
-    { nom: "C++", Image: "./img/cplusplus.svg" },
+    {nom: "Python", Image: "./img/Python--Streamline-Font-Awesome.svg" },
+    {nom: "SQL", Image: "./img/Sql--Streamline-Carbon.svg" },
+    { nom: "C++", Image: "./img/cplusplus.svg"},
     { nom: "SFML", Image: "./img/sfml.svg" },
     { nom: "Git", Image: "./img/github.svg" },
     { nom: "Figma pour le prototypage", Image: "./img/figma.svg" },
+    { nom: "Java", Image: "./img/Java--Streamline-Font-Awesome.svg" },
+    { nom: "HTML", Image: "./img/html5.svg" },
+    { nom: "CSS", Image: "./img/css.svg" },
+    { nom: "JavaScript", Image: "./img/javascript.svg" }
+    
   ];
